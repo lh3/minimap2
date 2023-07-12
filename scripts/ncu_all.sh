10e71977e53f6f121ed1a0133672b8170cd7cd78
Xueshen Liu <liuxs@umich.edu>
2023-07-12 10:49:27 -0400

    update parseing profile scripts

--- scripts/ncu_all.sh
+++ scripts/ncu_all.sh
@@ -26,11 +26,9 @@ do
                     # Build with specific configurations
                     make GPU=NVCC GPU_CONFIG=${GPU_CONFIG} SHORT_BLOCK_SIZE=64 MID_BLOCK_SIZE=${MID_BLOCK_SIZE} LONG_BLOCK_SIZE=512 MID_CUT=${MID_CUT} LONG_CUT=${LONG_CUT} 
                     
-                    # # Run nsys nvprof with the given command # data/random_500MBases_200kto300k.fa
-                    # read by `nsys stats --report cuda_gpu_kern_sum nsys/nvprofxxx_xxx_xxx.nsys-rep` 
+                    # # Run nsys ncu with the given command # data/random_500MBases_200kto300k.fa
+                    # read by `ncu --import ncu_midXXX_X_XXX_gpu_configX.json_XXXktoXXXk_report.ncu-rep --section SpeedOfLight --kernel-name score_generation_mid -c 1 --csv > test.csv` 
                     ncu --export "ncu/ncu32_mid${MID_BLOCK_SIZE}_${MID_CUT}_${LONG_CUT}_${GPU_CONFIG}_${DATA_SET}_report" --force-overwrite --target-processes all --kernel-name-base function --kernel-name regex:score_generation_mid --launch-count 1 --launch-skip-before-match 0 --section ComputeWorkloadAnalysis --section InstructionStats --section LaunchStats --section MemoryWorkloadAnalysis --section MemoryWorkloadAnalysis_Chart --section MemoryWorkloadAnalysis_Tables --section Occupancy --section SchedulerStats --section SourceCounters --section SpeedOfLight --section SpeedOfLight_RooflineChart --section WarpStateStats --sampling-interval auto --sampling-max-passes 5 --sampling-buffer-size 33554432 --profile-from-start 1 --cache-control all --clock-control base --apply-rules yes --import-source yes --source-folders gpu --check-exit-code yes ./minimap2 data/hg38.mmi data/random_500MBases_${DATA_SET}.fa -t 1 --max-chain-skip=2147483647 --gpu-chain # 2> ncu/runtime_report_mid${MID_BLOCK_SIZE}_${MID_CUT}_${LONG_CUT}_${DATA_SET}.txt
-                    # nsys nvprof -o nsys/nvprof_mid${MID_BLOCK_SIZE}_${MID_CUT}_${LONG_CUT} ./minimap2 data/hg38.mmi data/random_500MBases_200kto300k.fa -t 1 --max-chain-skip=2147483647 --gpu-chain 2> nsys/runtime_report_mid${MID_BLOCK_SIZE}_${MID_CUT}_${LONG_CUT}.txt
-                    # nsys stats --format csv --report cuda_gpu_kern_sum nsys/nvprofxxx_xxx_xxx.nsys-rep
                     echo "Done with MID_BLOCK_SIZE=${MID_BLOCK_SIZE} MID_CUT=${MID_CUT} LONG_CUT=${LONG_CUT}"
                 done
             done
